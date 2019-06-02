545c55c25974fc1a48049d2b8aa704b64566f741
Glauber Gomes <glaubergomes@unifei.edu.br>
2019-06-02 01:47:45 -0300

    Login Delegado
    Implementação do login de administrador (Delegado)

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,23 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+
+
+            switch ($guard) {
+                case 'admin':
+                return redirect()->route('admin.dashboard');
+                break;
+
+                case 'policial':
+                return redirect()->route('policial.dashboard');
+                break;
+
+                default:
+                return redirect('/home');
+                break;
+            }
+
+
         }
 
         return $next($request);
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+
+
+//Rotas Admin
+Route::get('/admin', 'AdminController@index')->name('admin.dashboard');
+Route::get('/admin/login', 'Auth\AdminLoginController@index')->name('admin.login');
+Route::post('/admin/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
